0141e188239947fba2b2ef39ce51f9dff1432a74
thefourCraft <me@thefourcraft.com>
2024-05-23 19:14:59 +0300

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   organizationName: 'tgilabs', // Usually your GitHub org/user name.
   projectName: 'public-docs', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
