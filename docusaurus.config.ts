94f194e8879fc680b8939c56457cf82bf7da855d
thefourCraft <me@thefourcraft.com>
2024-06-20 08:18:01 +0300

    renove blog from wiki

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -41,14 +41,7 @@ const config: Config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          blogSidebarTitle: 'כל העדכונים',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        blog: false,
         theme: {
           customCss: './src/css/custom.css',
         },
