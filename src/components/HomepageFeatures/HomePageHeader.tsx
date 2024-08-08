26b43aea6329adec2160667811c9538d72bb7666
Fravy <arirl.elkaslasi@gmail.com>
2024-08-08 17:21:11 +0300

    Homepage Header remake (changed scale + more features) made it more static

--- src/components/HomepageFeatures/HomePageHeader.tsx
+++ src/components/HomepageFeatures/HomePageHeader.tsx
@@ -4,11 +4,14 @@ import '@site/src/css/HomePage.css'
 
 export default function HomePageHeader(): JSX.Element {
     return (
-        <div className='Header-Container'>
-            <h2 className='text1' style={{direction:'rtl'}}> הויקיפדיה של TeGriAi</h2>
-            <p className='text2'>ברוכים הבאים לוויקיפדיה שלנו! הקהילה הישראלית הכי גדולה ברשת</p>
-            <button className='start-button'>מתחילים</button>
-        </div>
+    <div className='Header-Container'>
+    <div className='text-wrapper'>
+    <h2 className='text1'>הויקיפדיה של TeGriAi</h2>
+    <p className='text2'>ברוכים הבאים לוויקיפדיה שלנו! הקהילה הישראלית הכי גדולה ברשת</p>
+    </div>
+    <button className='start-button'>מתחילים</button>
+    </div>
+
     );
   } 
   
