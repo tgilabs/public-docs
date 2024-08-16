30e240ea710641c64c9a4edb04257d8a40579e26
ariel elkaslasi <099558er@gmail.com>
2024-08-16 16:34:54 +0300

    Adjustment to main page, Switch boxshadow to Parallax for 3d effect for the image when scrolling.

--- src/components/HomepageFeatures/HomePageHeader.tsx
+++ src/components/HomepageFeatures/HomePageHeader.tsx
@@ -1,18 +1,37 @@
-
-import '@site/src/css/custom.css'
-import '@site/src/css/HomePage.css'
+import React, { useEffect, useState } from 'react';
+import '@site/src/css/custom.css';
+import '@site/src/css/HomePage.css';
 
 export default function HomePageHeader(): JSX.Element {
-    return (
+  const [scrollPosition, setScrollPosition] = useState(0);
 
-    <div className='Header-Container'>
-    <div className='text-wrapper'>
-    <h2 className='text1'>הויקיפדיה של TeGriAi</h2>
-    <p className='text2'>ברוכים הבאים לוויקיפדיה שלנו! הקהילה הישראלית הכי גדולה ברשת</p>
-    </div>
-    <button className='start-button'>מתחילים</button>
-    </div>
-
-    );
-  } 
   
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const parallaxFactor = 0.2;
+  const backgroundPositionY = scrollPosition * parallaxFactor;
+
+  return (
+    <div
+      className='Header-Container'
+      style={{ backgroundPositionY: `${backgroundPositionY}px` }}
+    >
+      <div className='text-wrapper'>
+        <h2 className='text1'>הויקיפדיה של TeGriAi</h2>
+        <p className='text2'>ברוכים הבאים לוויקיפדיה שלנו! הקהילה הישראלית הכי גדולה ברשת</p>
+      </div>
+      <button className='start-button'>מתחילים</button>
+    </div>
+  );
+}
