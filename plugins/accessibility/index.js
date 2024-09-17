f6e16bd46ad649ac70d8fa22f73138315a8eec1a
thefourCraft <me@thefourcraft.com>
2024-09-17 19:46:01 +0300

    Update NagishLi plugin configuration and script injection paths

--- plugins/accessibility/index.js
+++ plugins/accessibility/index.js
@@ -10,13 +10,13 @@ module.exports = function () {
                 nl_pos = "bl";
                 nl_compact = "1";
                 nl_accordion = "1";
-                nl_dir = "/plugins/nagishli-files/";
+                nl_dir = "@site/plugins/nagishli-files/";
               `,
             },
             {
               tagName: 'script',
               attributes: {
-                src: '/plugins/nagishli.js',
+                src: '@site/plugins/nagishli.js',
                 charset: 'utf-8',
                 defer: true,
               },
