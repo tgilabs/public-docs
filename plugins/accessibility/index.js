7a038af21c767dc2dcb6a8feb9ec5160c22da6b1
thefourCraft <me@thefourcraft.com>
2024-09-17 19:44:26 +0300

    Add accessibility plugin configuration and inject NagishLi scripts

---
+++ plugins/accessibility/index.js
@@ -0,0 +1,29 @@
+module.exports = function () {
+    return {
+      name: 'accessibility-plugin',
+      injectHtmlTags() {
+        return {
+          postBodyTags: [
+            {
+              tagName: 'script',
+              innerHTML: `
+                nl_pos = "bl";
+                nl_compact = "1";
+                nl_accordion = "1";
+                nl_dir = "/plugins/nagishli-files/";
+              `,
+            },
+            {
+              tagName: 'script',
+              attributes: {
+                src: '/plugins/nagishli.js',
+                charset: 'utf-8',
+                defer: true,
+              },
+            },
+          ],
+        };
+      },
+    };
+  };
+  
